1c0c0a32ab12fa1c17e244760368b2b8fc3f1ef2
MuhammedMushfiqV <mushfiqmuhd707@gmail.com>
2020-08-11 10:40:02 +0530

    Updates

---
+++ precache-manifest.772f17c4b89e493ed28fe2af70ce8e88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8e1cb0471afbc29efc83aac7ee3402a",
+    "url": "/Amazon_clone/index.html"
+  },
+  {
+    "revision": "76f790553c3b0dd30444",
+    "url": "/Amazon_clone/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "2c556b1e1284b0ce1d24",
+    "url": "/Amazon_clone/static/css/main.b5f8ae34.chunk.css"
+  },
+  {
+    "revision": "76f790553c3b0dd30444",
+    "url": "/Amazon_clone/static/js/2.cecd6e63.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Amazon_clone/static/js/2.cecd6e63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c556b1e1284b0ce1d24",
+    "url": "/Amazon_clone/static/js/main.305eba44.chunk.js"
+  },
+  {
+    "revision": "31683a34cb8a44a21641",
+    "url": "/Amazon_clone/static/js/runtime-main.12570025.js"
+  }
+]);
